4fdef284496544f754ca7f65f62522548d224c50
Andi Hakim Arif <andi.hakim.arif@gmail.com>
2021-02-15 11:00:27 +0700

    virtual makeup gradient

--- src/components/makeup/index.js
+++ src/components/makeup/index.js
@@ -1,72 +1,144 @@
-import Engine from './components/engine';
-import { useState } from 'react';
-import { cssBackdrop, cssCard, cssControl, cssLoading } from './style';
-
+import Engine from "./components/engine";
+import { useState } from "react";
+import { cssBackdrop, cssCard, cssControl, cssLoading } from "./style";
 const skins = [
   null,
-  'rgba(229,194,152, 0.3)',
-  'rgba(204,132,67, 0.3)',
-]
-
+  {
+    name: "Color 1",
+    type: "color",
+    background: "rgba(229,194,152, 0.3)",
+  },
+  {
+    name: "Color 2",
+    type: "color",
+    background: "rgba(204,132,67, 0.3)",
+  },
+  {
+    name: "Gradient 1",
+    type: "gradient",
+    background:
+      ["rgba(255,255,255, 1)", "rgba(204,132,67, 0.3)"],
+  },
+];
 const lips = [
   null,
-  'rgba(225,0,0, 0.8)',
-  'rgba(0,0,225, 0.8)',
-]
-
+  {
+    name: "Color 1",
+    type: "color",
+    background: "rgba(225,0,0, 0.8)",
+  },
+  {
+    name: "Color 2",
+    type: "color",
+    background: "rgba(0,0,225, 0.8)",
+  },
+  {
+    name: "Gradient 1",
+    type: "gradient",
+    background:
+      ["rgba(255,255,255, 1)", "rgba(0,0,225, 0.8)"],
+  },
+];
 const shadow = [
   null,
-  'rgba(0,0,225, 0.7)',
-]
-
-
-const Makeup = ({onClose}) => {
-  const [ ready, setReady ] = useState(false);
-  const [ started, setStarted ] = useState(false);
+  {
+    name: "Color 1",
+    type: "color",
+    background: "rgba(0,0,225, 0.7)",
+  },
+  {
+    name: "Gradient 1",
+    type: "gradient",
+    background:
+      ["rgba(255,255,255, 1)", "rgba(0,0,225, 0.7)"],
+  },
+];
+const Makeup = ({ onClose }) => {
+  const [ready, setReady] = useState(false);
+  const [started, setStarted] = useState(false);
   const [activeSkin, setActiveSkin] = useState(0);
   const [activeLip, setActiveLip] = useState(0);
   const [activeEyeShadow, setActiveEyeShadow] = useState(0);
-
   const handleReady = () => {
     setReady(true);
-  }
-
+  };
   const handleStart = () => {
     setStarted(true);
-  }
-
+  };
   const handleClose = () => {
     ready && onClose();
-  }
-
+  };
   return (
     <>
-      <div className={cssBackdrop} onClick={handleClose}/>
+      <div className={cssBackdrop} onClick={handleClose} />
       <div className={cssCard}>
-        <Engine skin={skins[activeSkin]} lip={lips[activeLip]} eyeShadow={shadow[activeEyeShadow]} onReady={handleReady} />
+        <Engine
+          skin={skins[activeSkin]}
+          lip={lips[activeLip]}
+          eyeShadow={shadow[activeEyeShadow]}
+          onReady={handleReady}
+        />
         <div className={cssControl}>
           {skins.map((each, index) => {
-            return <button type="button" key={index} onClick={()=>{setActiveSkin(index)}}>{each ? `color-${index}` : 'nomakeup'}</button>
+            return (
+              <button
+                type="button"
+                key={index}
+                onClick={() => {
+                  setActiveSkin(index);
+                }}
+              >
+                {each?.name || "nomakeup"}
+              </button>
+            );
           })}
-          <br/>
+          <br />
           {shadow.map((each, index) => {
-            return <button type="button" key={index} onClick={()=>{setActiveEyeShadow(index)}}>{each ? `color-${index}` : 'noShadow'}</button>
+            return (
+              <button
+                type="button"
+                key={index}
+                onClick={() => {
+                  setActiveEyeShadow(index);
+                }}
+              >
+                {each?.name || "noShadow"}
+              </button>
+            );
           })}
-          <br/>
+          <br />
           {lips.map((each, index) => {
-            return <button type="button" key={index} onClick={()=>{setActiveLip(index)}}>{each ? `color-${index}` : 'noLipstick'}</button>
+            return (
+              <button
+                type="button"
+                key={index}
+                onClick={() => {
+                  setActiveLip(index);
+                }}
+              >
+                {each?.name || "noLipstick"}
+              </button>
+            );
           })}
         </div>
         <div className={cssLoading(started)}>
           <div>
             <h3>Virtual TryOn</h3>
-            <p>You can try-on lipstick, eyeshadow, and foundation virtually. Please allow your webcam access permission to try this feature.</p>
-            {!ready ? 'LOADING...' : <button type="button" onClick={handleStart}>GETTING STARTED</button>}
+            <p>
+              You can try-on lipstick, eyeshadow, and foundation virtually.
+              Please allow your webcam access permission to try this feature.
+            </p>
+            {!ready ? (
+              "LOADING..."
+            ) : (
+              <button type="button" onClick={handleStart}>
+                GETTING STARTED
+              </button>
+            )}
           </div>
         </div>
       </div>
     </>
   );
-}
-
+};
 export default Makeup;
--- src/components/makeup/methods/utils.js
+++ src/components/makeup/methods/utils.js
@@ -1,6 +1,5 @@
 import TRIANGULATION from '../const/triangulation';
 import { lipMesh, rightEyeMesh, rightEyeShadowMesh, leftEyeMesh, leftEyeShadowMesh } from '../const/segmentation';
-
 /* canvas drawing path method */
 const drawPath = (ctx, points, closePath) => {
   const region = new Path2D();
@@ -9,25 +8,21 @@ const drawPath = (ctx, points, closePath) => {
     const point = points[i];
     region.lineTo(point[0], point[1]);
   }
-
   if (closePath) {
     region.closePath();
   }
   ctx.stroke(region);
   ctx.fill(region);
 }
-
 export const paintingFace = (ctx, predictions, {skin, lip, eyeShadow}) => {
   let lipFlag = 0;
   let rightEyeFlag = 0;
   let rightEyeShadowFlag = 0;
   let leftEyeShadowFlag = 0;
   let leftEyeFlag = 0;
-
   if (predictions.length > 0) {
     predictions.forEach(prediction => {
       const keypoints = prediction.scaledMesh;
-
       /* loop all triangulation face-map and match to prediction points */
       for (let i = 0; i < TRIANGULATION.length; i++) {
         const points = [
@@ -35,11 +30,12 @@ export const paintingFace = (ctx, predictions, {skin, lip, eyeShadow}) => {
           TRIANGULATION[i][1],
           TRIANGULATION[i][2]
         ].map(index => keypoints[index]);
-
         //seperate face segmentation and set each segment's color
-        let drawColor = skin;
+        let drawColor = skin?.background || null;
+        let drawType = skin?.type || null;
         if (lipMesh[lipFlag] === i){
-          drawColor = lip;
+          drawColor = lip?.background;
+          drawType = lip?.type;
           lipFlag += 1;
         }
         else if(rightEyeMesh[rightEyeFlag] === i){
@@ -47,7 +43,8 @@ export const paintingFace = (ctx, predictions, {skin, lip, eyeShadow}) => {
           rightEyeFlag += 1;
         }
         else if(eyeShadow && rightEyeShadowMesh[rightEyeShadowFlag] === i){
-          drawColor = eyeShadow;
+          drawColor = eyeShadow?.background;
+          drawType = eyeShadow?.type;
           rightEyeShadowFlag += 1;
         }
         else if(leftEyeMesh[leftEyeFlag] === i){
@@ -55,12 +52,19 @@ export const paintingFace = (ctx, predictions, {skin, lip, eyeShadow}) => {
           leftEyeFlag += 1;
         }
         else if(eyeShadow && leftEyeShadowMesh[leftEyeShadowFlag] === i){
-          drawColor = eyeShadow;
+          drawColor = eyeShadow?.background;
+          drawType = eyeShadow?.type;
           leftEyeShadowFlag += 1;
         }
-
         /* do drawPath method if the color is not null */
         if(drawColor){
+          if(drawType === 'gradient') {
+            var grd = ctx.createLinearGradient(0, 0, 0, ctx.canvas.height);
+            // var grd = ctx.createRadialGradient(238, 50, 10, 238, 50, 300);
+            grd.addColorStop(0, drawColor?.[0]);
+            grd.addColorStop(1, drawColor?.[1]);
+            drawColor = grd
+          };
           ctx.fillStyle = drawColor;
           ctx.strokeStyle = drawColor;
           drawPath(ctx, points, true)
